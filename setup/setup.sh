54b79c5bd669105bd3160f397373753b7f9a5270
InnovativeInventor <theinnovativeinventor@gmail.com>
2019-03-05 17:05:14 -0500

    Renamed setup to fish-setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,4 +1,4 @@
-sudo apt-get install software-properties-common -y
+sudo apt-get install software-properties-common curl -y
 sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt-get update
 sudo apt-get install neovim -y
@@ -11,5 +11,6 @@ vim +PluginInstall +qall
 mkdir -p ~/.Config
 mv -n dotfiles/fish ~/.Config/fish
 curl -L https://get.oh-my.fish | fish
-sudo rm -r dotfiles
-fish setup.fish
+curl -L https://raw.githubusercontent.com/InnovativeInventor/boilerplate/master/setup/fish-setup.fish
+fish fish-setup.fish
+sudo rm -r dotfiles fish-setup.fish
